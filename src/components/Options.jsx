5682e2b06d93ad8248161f323d88930ee46b84c2
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-03-09 11:11:53 -0800

    options mejora

--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -1,24 +1,14 @@
 export default function Options() {
   return (
     <div className="flex justify-center">
-      {" "}
-      <span className="isolate inline-flex rounded-md shadow-sm">
-        <button
-          type="button"
-          className="relative inline-flex items-center rounded-l-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-        >
+      <span className="flex rounded-full py-3 bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+        <button type="button" className="px-5">
           Mis proyectos
         </button>
-        <button
-          type="button"
-          className="relative -ml-px inline-flex items-center bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-        >
+        <button type="button" className="px-5">
           Experiencias
         </button>
-        <button
-          type="button"
-          className="relative -ml-px inline-flex items-center rounded-r-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-10"
-        >
+        <button type="button" className="px-5">
           Educación
         </button>
       </span>
