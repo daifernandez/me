20afcb27b0c9541bff8846dd82a2cf4f232cf773
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-03-22 19:46:44 -0700

    projects col2

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -47,7 +47,7 @@ export default function Projects() {
             uno para ver más detalles
           </p>
         </div>
-        <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+        <div className="mx-auto mt-16 grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 sm:mt-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           {posts.map((post) => (
             <article
               key={post.id}
