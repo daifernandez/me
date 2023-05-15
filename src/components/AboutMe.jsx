a7a14767798ec82e8e22b4cf4bd590b04d19d22e
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-05-15 18:00:06 -0300

    dark mode about me arreglo detalles

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -17,7 +17,7 @@ export default function AboutMe() {
       </div>
       <div className="relative isolate overflow-hidden bg-gradient-to-b from-indigo-100/20 pt-14">
         <div
-          className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white  dark:bg-slate-900 shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
+          className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white  dark:bg-slate-900 shadow-xl shadow-indigo-600/10 dark:shadow-slate-800 ring-1 ring-indigo-50 dark:ring-slate-800 sm:-mr-80 lg:-mr-96"
           aria-hidden="true"
         />
         <section id="cv" className="px-6 py-12 bg-gray-50 dark:bg-slate-900">
