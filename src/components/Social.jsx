4f6a37d45aa5319c3db2cbcb7dc6588d7621d2c2
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-06-08 18:31:07 -0300

    arreglo social links

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function Social() {
   return (
     <div className="flex flex-row mt-5">
-      <Link
-        to="https://github.com/daifernandez"
+      <a
+        href="https://github.com/daifernandez"
         target="_blank"
         rel="noopener noreferrer"
         className="flex flex-row mt-5 transform transition duration-500 ease-in-out hover:scale-110"
@@ -24,9 +24,9 @@ export default function Social() {
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M9 19c-4.3 1.4 -4.3 -2.5 -6 -3m12 5v-3.5c0 -1 .1 -1.4 -.5 -2c2.8 -.3 5.5 -1.4 5.5 -6a4.6 4.6 0 0 0 -1.3 -3.2a4.2 4.2 0 0 0 -.1 -3.2s-1.1 -.3 -3.5 1.3a12.3 12.3 0 0 0 -6.2 0c-2.4 -1.6 -3.5 -1.3 -3.5 -1.3a4.2 4.2 0 0 0 -.1 3.2a4.6 4.6 0 0 0 -1.3 3.2c0 4.6 2.7 5.7 5.5 6c-.6 .6 -.6 1.2 -.5 2v3.5" />
         </svg>
-      </Link>
-      <Link
-        to="https://twitter.com/daiofernandez"
+      </a>
+      <a
+        href="https://twitter.com/daiofernandez"
         target="_blank"
         rel="noopener noreferrer"
         className="flex flex-row mt-5 transform transition duration-500 ease-in-out hover:scale-110"
@@ -46,9 +46,9 @@ export default function Social() {
           <path stroke="none" d="M0 0h24v24H0z" fill="none" />
           <path d="M22 4.01c-1 .49 -1.98 .689 -3 .99c-1.121 -1.265 -2.783 -1.335 -4.38 -.737s-2.643 2.06 -2.62 3.737v1c-3.245 .083 -6.135 -1.395 -8 -4c0 0 -4.182 7.433 4 11c-1.872 1.247 -3.739 2.088 -6 2c3.308 1.803 6.913 2.423 10.034 1.517c3.58 -1.04 6.522 -3.723 7.651 -7.742a13.84 13.84 0 0 0 .497 -3.753c-.002 -.249 1.51 -2.772 1.818 -4.013z" />
         </svg>
-      </Link>
-      <Link
-        to="https://www.linkedin.com/in/daifernandez/"
+      </a>
+      <a
+        href="https://www.linkedin.com/in/daifernandez/"
         target="_blank"
         rel="noopener noreferrer"
         className="flex flex-row mt-5 transform transition duration-500 ease-in-out hover:scale-110"
@@ -72,7 +72,7 @@ export default function Social() {
           <line x1="12" y1="16" x2="12" y2="11" />
           <path d="M16 16v-3a2 2 0 0 0 -4 0" />
         </svg>
-      </Link>
+      </a>
       <Link
         to="#"
         className="flex flex-row mt-5 transform transition duration-500 ease-in-out hover:scale-110"
