f63dbe2f378bd5d2c44f74088e9530f711149fb4
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-08-09 15:22:33 -0300

    animacion en projectDelails

--- src/components/ProjectDetails.jsx
+++ src/components/ProjectDetails.jsx
@@ -153,16 +153,16 @@ export default function ProjectDetails() {
         <div className="animate-fade animate-once animate-duration-1000 animate-ease-in mx-auto max-w-7xl px-6 lg:flex lg:px-8">
           <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-12 gap-y-16 lg:mx-0 lg:min-w-full lg:max-w-none lg:flex-none lg:gap-y-8">
             <div className="lg:col-end-1 lg:w-full lg:max-w-lg lg:pb-8">
-              <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-200 sm:text-4xl">
+              <h2 className="animate-fade-up text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-200 sm:text-4xl">
                 {project.name}
               </h2>
               <p
-                className="mt-6 text-xl leading-8 text-gray-600 dark:text-gray-400"
+                className="animate-fade-left mt-6 text-xl leading-8 text-gray-600 dark:text-gray-400"
                 style={{ textAlign: "justify" }}
               >
                 {project.description}
               </p>
-              <section className="mt-12">
+              <section className="animate-fade-left mt-12">
                 <h2 className="text-lg font-medium text-gray-900 dark:text-indigo-600 mb-4">
                   Tecnologías utilizadas
                 </h2>
