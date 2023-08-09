04e68c36bb17b74a1f895117b5d5711d61d456a7
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-08-09 15:25:38 -0300

    animacion en Intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -9,13 +9,13 @@ export default function Intro() {
     <div className="py-12 sm:py-12">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
-          <p className="text-base font-semibold leading-7 text-indigo-600">
+          <p className="animate-fade-left text-base font-semibold leading-7 text-indigo-600">
             Bienvenido a mi portfolio!
           </p>
           <h2 className="animate-fade animate-once animate-duration-[10000ms] mt-2 text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl dark:text-white">
             FullStack Web Developer
           </h2>
-          <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-400">
+          <p className="animate-fade-left mt-6 text-lg leading-8 text-gray-600 dark:text-gray-400">
             Hola mi nombre es Daiana, soy desarrolladora web fullstack. Aquí encontrarás algunos de mis proyectos más recientes. Me
             enorgullece compartir mi trabajo y espero que puedas encontrar algo
             que te inspire o te resulte útil. Si tienes alguna pregunta o
@@ -24,7 +24,7 @@ export default function Intro() {
           <button
             type="button"
             onClick={() => redirect.push("/me")}
-            className="text-sm mt-8 font-semibold leading-6 text-gray-900 dark:text-white hover:text-indigo-600 dark:hover:text-indigo-600"
+            className="animate-fade-left text-sm mt-8 font-semibold leading-6 text-gray-900 dark:text-white hover:text-indigo-600 dark:hover:text-indigo-600"
           >
             Más sobre Mi <span aria-hidden="true">→</span>
           </button>
