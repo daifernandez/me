cc23e5e57ecbb3e917d1bb7d5afa9e94ca643769
Daiana Fernandez <dtama.fernandez@gmail.com>
2023-10-03 11:52:12 -0300

    Tech popover button cambio etiqueta a

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -128,13 +128,13 @@ export default function Tech() {
         >
           <p className="relative rounded-full bg-indigo-100/20 dark:bg-slate-800 px-4 py-1.5 text-sm leading-6 text-gray-600 ring-1 ring-inset ring-gray-900/5ring-1 ring-zinc-900/5 backdrop-blur dark:text-zinc-200 dark:ring-white/10  shadow-zinc-800/5">
             <span className="hidden md:inline">Chequea las</span>
-            <a
+            <span
               href="#"
               className="font-semibold text-indigo-600 dark:text-indigo-500"
             >
               <span className="absolute inset-0" aria-hidden="true" />{" "}
               Tecnologias que manejo <span aria-hidden="true">&rarr;</span>
-            </a>
+            </span>
           </p>
         </Popover.Button>
 
